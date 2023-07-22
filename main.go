8410b6d48d87087e328b84db294efd12fcb9558b
haydark03 <kamilhaydar03@gmail.com>
2023-07-22 16:49:59 +0700

    add declaration

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 func main() {
 	hello()
+	//ini boolean
 	boolean()
+	//ini constanta
 	constant()
+	//ini konversi
 	tipekonversi()
+	//ini number
 	number()
+	//ini string
 	tipestring()
+	//ini variable
 	variable()
+	//ini declaration
+	declaration()
 }
