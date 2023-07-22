92cb6cb2aaf180866866c3545822bf632d6e6489
haydark03 <kamilhaydar03@gmail.com>
2023-07-22 17:21:30 +0700

    add main

---
+++ nextLearn/math-Op.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result = 19 + 20
+	fmt.Println(result)
+
+	var a = 10
+	var b = 20
+	var c = a * b
+
+	fmt.Println(c)
+
+	//augmented assigments
+	var i = 30
+	i += 10
+	fmt.Println(i)
+
+	var d = 20
+	d -= 10
+	fmt.Println(d)
+
+	var e = 10
+	e *= 10
+	fmt.Println(e)
+
+	var f = 100
+	f /= 10
+	fmt.Println(f)
+
+	var g = 20
+	g %= 10
+	fmt.Println(g)
+
+}
