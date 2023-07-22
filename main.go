1f65af82383fdf9028bb075433837d548e31424f
haydark03 <kamilhaydar03@gmail.com>
2023-07-22 13:06:42 +0700

    undo comment

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 func main() {
-	//ini hello
 	hello()
-	//ini number
-	number()
-	//ini boolean
 	boolean()
-	//ini string
-	tipestring()
-	//ini variable
-	variable()
-	//ini constant
 	constant()
-	//konversi tipe data
 	tipekonversi()
+	number()
+	tipestring()
+	variable()
 }
